2579716f8e44d3583bfabf4c288f11b649901c13
audreyeg <63382246+audreyeg@users.noreply.github.com>
2020-05-01 12:19:33 -0700

    First commit

---
+++ TestProject/src/Hello.java
@@ -0,0 +1,14 @@
+/*
+ * this is a test to see if everyone can use git and eclipse 
+ * please pull this into your system, comment your name, and push
+ * it back into the repository so we know everyone can do it :) 
+ */
+
+// Ella Gainey
+
+public class Hello {
+		
+	public static void main (String theArgs[]) {
+	System.out.println("TEAM SASS");
+	}
+}
