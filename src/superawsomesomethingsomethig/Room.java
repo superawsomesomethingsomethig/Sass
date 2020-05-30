c036d80027f606603a68340fbec40dcb0976005b
supersam710 <spillers.sam@gmail.com>
2020-05-29 21:44:28 -0700

    Added model classes and loading/saving

--- src/superawsomesomethingsomethig/Room.java
+++ src/superawsomesomethingsomethig/Room.java
@@ -1,24 +1,51 @@
 package superawsomesomethingsomethig;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.List;
 
-public class Room 
+public class Room implements Serializable
 {
-	private Map<String,Appliance> applianceList;
+	/**
+	 * Default Serial UID for Room
+	 */
+	private static final long serialVersionUID = 2L;
+	private List<Appliance> applianceList;
+	private String name;
 	
-	public Room()
+	public Room(String name)
 	{
-		applianceList = new HashMap<>();
+		applianceList = new LinkedList<>();
+		this.name = name;
 	}
-	//retrieve appliances and build the list.
-	private void BuildList()
-	{
-		
+	public List<Appliance> getList() {
+		return new LinkedList<Appliance>(applianceList);  // List is copied to avoid editing errors
 	}
-	//retrieve a specified Appliance
-	public Appliance getAppliance(String name)
-	{
-		
+	public Appliance create(String applianceName) {
+		Appliance appliance = new Appliance(applianceName);
+		applianceList.add(appliance);
+		return appliance;
+	}
+	public void create(Appliance appliance) {
+		applianceList.add(appliance);
+	}
+	public void destroy(Appliance appliance) {
+		applianceList.remove(appliance);
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String toString() {
+		String output = "Room(\"" + name + "\")[ ";
+		for (int i = 0; i < applianceList.size(); i++) {
+			output += applianceList.get(i).toString();
+			if (i < applianceList.size() - 1) {  // Fencepost check
+				output += ", ";
+			}
+		}
+		return output + " ]";
 	}
 }
