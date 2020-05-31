d4b9c36d3fb932d12ef4168e090346af5ffdbecc
Timmy <Timmy@DESKTOP-5J8U29F>
2020-05-31 13:26:52 -0700

    added the get room and implemented save and load properly

--- src/superawsomesomethingsomethig/AppMain.java
+++ src/superawsomesomethingsomethig/AppMain.java
@@ -6,30 +6,7 @@ import java.io.IOException;
 public class AppMain 
 {
 	public static void main(String[] args) 
-	{			
-		
-		// TODO: Delete the following. It is for saving/loading testing
-		House house = null;
-		try {
-			house = House.loadHouse("houseFile.hf");
-		} catch (IOException | ClassNotFoundException e) {
-			house = new House();
-		}
-
-		Room room = house.create("testRoom2");
-		Appliance appliance = room.create("testAppliance2");
-		appliance.create("testDocument2");
-		System.out.println(house);
-		
-		try {
-			House.saveHouse(house, "houseFile.hf");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		// TODO: Delete the above. It is for saving/loading testing
-
-		
-				
+	{				
 		HomeScreen homeScreen = new HomeScreen();
 		homeScreen.setVisible(true);
 	}
